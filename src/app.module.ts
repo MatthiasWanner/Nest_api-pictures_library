a008fae547976d586ce268eca5b7dd9d2f73f704
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-13 07:32:12 +0200

    Create PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PrismaService],
   controllers: [AppController],
   providers: [AppService],
 })
