c8710a3631b4c0f01a04a22abb499358dc285a32
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-29 20:39:13 +0200

    Ignore dist folder from git

--- dist/users/dto/create-user.dto.d.ts
+++
@@ -1,7 +0,0 @@
-declare class CreateUserDto {
-    readonly username: string;
-    readonly email: string;
-    readonly role: 'ADMIN' | 'USER';
-    readonly password: string;
-}
-export { CreateUserDto };
