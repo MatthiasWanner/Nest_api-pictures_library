27e258f89081ac21f81d8bd35fb8487507896515
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-29 16:41:54 +0200

    Typing user dto and api response class. Have to fix why a type error occured in terminal when user class extends create dto class

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsEmail, IsEnum, IsString } from 'class-validator';
+import { UserRole } from '@users/entities/user.entity';
+
+class CreateUserDto {
+  @ApiProperty({
+    required: true,
+  })
+  @IsString()
+  @Type(() => String)
+  readonly username: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsEmail()
+  @Type(() => String)
+  readonly email: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsEnum(UserRole)
+  readonly role: 'ADMIN' | 'USER';
+
+  @ApiProperty({
+    required: true,
+  })
+  @Type(() => String)
+  @IsString()
+  readonly password: string;
+}
+export { CreateUserDto };
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+enum UserRole {
+  ADMIN,
+  USER,
+}
+
+class User {
+  @ApiProperty()
+  @IsString()
+  readonly id: string;
+
+  @ApiProperty()
+  @IsString()
+  readonly createdAt: Date;
+
+  @ApiProperty()
+  @IsString()
+  readonly updatedAt: Date;
+
+  // extends from CreateUserDto but return a tyerror
+  @ApiProperty({
+    required: true,
+  })
+  @IsString()
+  @Type(() => String)
+  readonly username: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsEmail()
+  @Type(() => String)
+  readonly email: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsEnum(UserRole)
+  readonly role: 'ADMIN' | 'USER';
+}
+export { User, UserRole };
