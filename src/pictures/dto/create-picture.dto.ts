bd0a67ba5f43d39e018717c0721a8523760b2971
MatthiasWanner <matthias.wanner@gmail.com>
2021-09-05 21:13:07 +0200

    Create pictures classes (entity and dtos)

--- src/pictures/dto/create-picture.dto.ts
+++ src/pictures/dto/create-picture.dto.ts
@@ -1,17 +1,8 @@
-export class CreatePictureDto {}
+import { OmitType } from '@nestjs/swagger';
+import { Picture } from '../entities/picture.entity';
 
-/* 
-model Picture {
-  id          String   @id @default(uuid())
-  title       String
-  description String?
-  url         String
-  profile     Profile  @relation(fields: [profileId], references: [id])
-  profileId   String   @map(name: "profile_id")
-  createdAt   DateTime @default(now()) @map(name: "created_at")
-  updatedAt   DateTime @default(now()) @map(name: "updated_at")
-  albums      Album[]
-
-  @@map(name: "pictures")
-}
-*/
+export class CreatePictureDto extends OmitType(Picture, [
+  'id',
+  'createdAt',
+  'updatedAt',
+]) {}
--- src/pictures/entities/picture.entity.ts
+++ src/pictures/entities/picture.entity.ts
@@ -1,1 +1,45 @@
-export class Picture {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsString } from 'class-validator';
+
+export class Picture {
+  @ApiProperty()
+  @IsString()
+  readonly id!: string;
+
+  @ApiProperty()
+  @IsString()
+  readonly createdAt!: Date;
+
+  @ApiProperty()
+  @IsString()
+  readonly updatedAt!: Date;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsString()
+  @Type(() => String)
+  readonly title!: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsString()
+  @Type(() => String)
+  readonly description?: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsString()
+  @Type(() => String)
+  readonly url!: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsString()
+  @Type(() => String)
+  readonly profileId!: string; // TODO: this field will not exist when auth providing api
+}
